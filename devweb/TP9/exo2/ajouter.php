1db4b22530eb83292925f371a26aeeaa907a3102
Quentin <quentin@LAPTOP-QUP4NKFQ.localdomain>
2022-04-14 01:55:01 +0200

    prout

--- devweb/TP9/exo2/ajouter.php
+++ devweb/TP9/exo2/ajouter.php
@@ -4,17 +4,16 @@
   $dataDeReal = $_POST["dateDeReal"];
   $duree = $_POST["duree"];
   $newFilm = array("titre"=>$title, "realisateur"=>$realisateur, "annee de realisation"=>$dataDeReal, "duree"=>$duree);
-  //$newfilmjson = json_encode($newFilm);
-  //echo $newfilmjson;
-  /*file_put_contents("mediatheque.json", $newfilmjson, FILE_APPEND | LOCK_EX);*/
-
-  //$newfilmjson = json_encode($film);
+  
   $oldJson = file_get_contents("mediatheque.json");
   $oldFilm = json_decode($oldJson, true);
   array_push($oldFilm, $newFilm);
   $newListejson = json_encode($oldFilm);
   //echo $newListejson;
   file_put_contents("mediatheque.json", $newListejson);
+
+
+  
   $listeFilm = file_get_contents("mediatheque.json");
   $film = json_decode($listeFilm, true);
 
