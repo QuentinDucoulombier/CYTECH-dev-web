07d7d8bc1275ecfbc393af59e09c85255f9324a4
QuentinDucoulombier <quent1ducou@gmail.com>
2023-03-21 23:49:49 +0100

    Fini td2

--- INGdevWeb/ING1GI3-DUCOULOMBIER-QUENTIN_temp/js/panier.js
+++ INGdevWeb/ING1GI3-DUCOULOMBIER-QUENTIN_temp/js/panier.js
@@ -2,22 +2,22 @@
 const plusButtons = document.querySelectorAll(".plus");
 const minusButtons = document.querySelectorAll(".minus");
 
-/*TODO:Desactiver le bouton de base*/
+/*Desactive le bouton de base*/
+minusButtons.forEach(button => {
+  button.disabled = true;
+});
 
+/*Autre cas*/
 plusButtons.forEach(button => {
   button.addEventListener("click", () => {
     
     const direction = button.getAttribute("data-direction");
     const stock = parseInt(document.querySelector(`tr td.stock[data-direction="${direction}"]`).textContent);
-    
-   
     const quantity = button.parentElement.querySelector(".quantity");
-    
-   
+
     let currentQuantity = parseInt(quantity.value);
     currentQuantity++;
-    
-    
+
     quantity.value = currentQuantity;
     if (currentQuantity >= stock) {
       button.disabled = true;
@@ -33,20 +33,15 @@ minusButtons.forEach(button => {
     const direction = button.getAttribute("data-direction");
     const stock = parseInt(document.querySelector(`tr td.stock[data-direction="${direction}"]`).textContent);
     
-  
     const quantity = button.parentElement.querySelector(".quantity");
-    
-    // Décrémentation de la quantité
     let currentQuantity = parseInt(quantity.value);
     currentQuantity--;
-    
-  
+
     quantity.value = currentQuantity;
     if (currentQuantity <= 0) {
       button.disabled = true;
     }
-    
-    
+
     button.parentElement.querySelector(".plus").disabled = false;
   });
 });
--- INGdevWeb/ING1GI3-DUCOULOMBIER-QUENTIN_temp/js/scriptForm.js
+++ INGdevWeb/ING1GI3-DUCOULOMBIER-QUENTIN_temp/js/scriptForm.js
@@ -1,50 +1,63 @@
 function validateForm() {
-    // Récupération des valeurs saisies dans les champs
+
     var nom = document.getElementById("nom").value;
     var prenom = document.getElementById("prenom").value;
-    var telephone = document.getElementById("Telephone").value;
+    var tel = document.getElementById("Telephone").value;
     var email = document.getElementById("Email").value;
-    var dateReservation = document.getElementById("dReserva").value;
-
-    // Expression régulière pour vérifier le format de l'email
-    var emailRegex = /^\S+@\S+\.\S+$/;
-    //TODO:Finir
-    /*
-    var nameRegex = /^[a-zA-Z ]+$/;
-
-    if (!(/^\d{10}$/.test(nom))) {
-        document.getElementById("Nom").style.borderColor = "red";
-        document.getElementById("Nom").value = telephone;
-        document.getElementById("Nom").style.display = "inline";
-        return false;
-    } else {
-        document.getElementById("Nom").style.borderColor = "";
-        document.getElementById("Nom").style.display = "none";
+    var dateReserv = document.getElementById("dReserva").value;
+    var sujetMail = document.getElementById("sMail").value;
+    var contenuMail = document.getElementById("cMail").value;
+    var genre = document.querySelector('input[name="Genre"]:checked');
+  
+    if (nom == "") {
+      document.getElementById("nom").style.borderColor = "red";
+      return false;
     }
-    */
-
-    // Vérification du format du numéro de téléphone
-    if (!(/^\d{10}$/.test(telephone))) {
-        document.getElementById("Telephone").style.borderColor = "red";
-        document.getElementById("Telephone").value = telephone;
-        document.getElementById("TelephoneFormat").style.display = "inline";
+  
+    if (prenom == "") {
+      document.getElementById("prenom").style.borderColor = "red";
+      return false;
+    }
+  
+    var telFormat = /^((\+)33|0)[1-9](\d{2}){4}$/;
+    if (!tel.match(telFormat)) {
+      document.getElementById("Telephone").style.borderColor = "red";
+      document.getElementById("TelephoneFormat").style.display = "inline";
+      return false;
+    }
+  
+    
+    var emailFormat = /^\S+@\S+\.\S+$/; 
+    if (!email.match(emailFormat)) {
+      document.getElementById("Email").style.borderColor = "red";
+      document.getElementById("EmailFormat").style.display = "inline";
+      return false;
+    }
+  
+    /*On peut pas reserver a une date precedente*/
+    var now = new Date();
+    console.log(now);
+    if (reservationDate < now) {
+        document.getElementById("dReserva").style.borderColor = "red";
+        document.getElementById("DateReservaError").style.display = "inline";
         return false;
-    } else {
-        document.getElementById("Telephone").style.borderColor = "";
-        document.getElementById("TelephoneFormat").style.display = "none";
+    }
+  
+    if (sujetMail == "") {
+      document.getElementById("sMail").style.borderColor = "red";
+      return false;
+    }
+  
+    if (contenuMail == "" || contenuMail == "Veuillez saisir le sujet du mail.") {
+      document.getElementById("cMail").style.borderColor = "red";
+      return false;
     }
 
-    // Vérification du format de l'email
-    if (!emailRegex.test(email)) {
-        document.getElementById("Email").style.borderColor = "red";
-        document.getElementById("Email").value = email;
-        document.getElementById("EmailFormat").style.display = "inline";
-        return false;
-    } else {
-        document.getElementById("Email").style.borderColor = "";
-        document.getElementById("EmailFormat").style.display = "none";
+    if (genre === null) {
+      document.getElementById("MainF").querySelectorAll('label')[8].style.color = "red";
+      return false;
     }
-
-    // Si toutes les données sont valides, on renvoie true
+  
     return true;
-}
+  }
+  
